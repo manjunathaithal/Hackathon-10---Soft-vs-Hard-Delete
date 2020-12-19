28e98a4b2ea552c22cdde9269c5e87feafea621f
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-19 18:43:32 +0530

    DOne

--- src/app.js
+++ src/app.js
@@ -10,8 +10,12 @@ app.use(express.json());
 
 // Get all the students
 app.get("/students", async (req, res) => {
-  const studentData = await Student.find();
-  res.send(studentData);
+  try {
+    const studentData = await Student.find();
+    res.send(studentData);
+  } catch (err) {
+    res.status(404).send(err);
+  }
 });
 
 // Add student to database
@@ -30,34 +34,42 @@ app.post("/students", async (req, res) => {
     await studentData.save();
     res.send(studentData);
   } catch (error) {
-    res.status(404).send("error");
+    res.status(404).send(error);
   }
 });
 
 // Get specific student
 app.get("/students/:id", async (req, res) => {
   // write your codes here
-  const id = req.params.id;
-  const studentData = await Student.findById(id);
-  res.send(studentData);
+  try {
+    const id = req.params.id;
+    const studentData = await Student.findById(id);
+    res.send(studentData);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // delete specific student
 app.delete("/students/:id", async (req, res) => {
   // write your codes here
-  const id = req.params.id;
-  const type = req.query.type;
-  console.log(id, type);
-  if (type === "soft") {
-    // const value = true;
-    const studentData = await Student.findById(id);
+  try {
+    const id = req.params.id;
+    const type = req.query.type;
+    console.log(id, type);
+    if (type === "soft") {
+      // const value = true;
+      const studentData = await Student.findById(id);
 
-    studentData.isDeleted = "true";
-    studentData.save();
-    res.send("Data is Updated");
-  } else {
-    Student.collection.drop();
-    res.send("Collection is Dropped");
+      studentData.isDeleted = "true";
+      studentData.save();
+      res.send("Data is Updated");
+    } else if (type === "hard") {
+      Student.collection.drop();
+      res.send("Collection is Dropped");
+    }
+  } catch (err) {
+    res.status(404).send(err);
   }
 });
 
